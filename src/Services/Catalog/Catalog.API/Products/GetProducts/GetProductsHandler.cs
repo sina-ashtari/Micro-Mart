1017e4f741675bfe8cc5b0d83b7bc13e760e3bfd
sina-ashtari <sinaa1383@gmail.com>
2026-07-06 21:02:52 +0330

    Feat : Add Pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.API.Products.GetProducts;
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsQueryHandler(IDocumentSession sessions) : IQueryHandler<GetProductsQuery, GetProductsResult>
@@ -8,7 +8,8 @@ internal class GetProductsQueryHandler(IDocumentSession sessions) : IQueryHandle
     public async Task<GetProductsResult> Handle(GetProductsQuery Query, CancellationToken cancellationToken)
     {
 
-        var products = await sessions.Query<Product>().ToListAsync(cancellationToken);
+        var products = await sessions.Query<Product>().ToPagedListAsync(Query.PageNumber ?? 1, Query.PageSize ?? 10, cancellationToken);
+
         return new GetProductsResult(products);
     }
 }

7d94c5f97bd8161ecd11edcec97e330088863e60
sina-ashtari <sinaa1383@gmail.com>
2026-07-09 14:30:35 +0330

    feat : implement redis caching

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<bool> DeleteBasket(string UserName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(UserName, cancellationToken);
+        await cache.RemoveAsync(UserName, cancellationToken);
+        return true;
+    }
+
+    public async Task<ShoppingCart> GetBasket(string UserName, CancellationToken cancellationToken = default)
+    {
+        var cachedBasket = await cache.GetStringAsync(UserName, cancellationToken);
+
+        if (!string.IsNullOrEmpty(cachedBasket)) return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket) ?? new();
+
+        var basket = await repository.GetBasket(UserName, cancellationToken);
+        await cache.SetStringAsync(UserName, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken CancellationToken = default)
+    {
+        await repository.StoreBasket(basket, CancellationToken);
+
+        await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), CancellationToken);
+
+        return basket;
+
+    }
+}
